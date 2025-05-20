d6281cb03a034b4879b9f93c6f0122a5f77000c3
aditisaran26 <aditi.saran123@gmail.com>
2025-05-20 13:14:55 +0530

    Initial Commit

---
+++ WeatherAppMockData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherAppMockData
+//
+//  Created by CEPL on 20/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
